9ba8e14aa81f3e1a579d4b542612da5327d5e4e0
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-10-13 22:26:59 +0300

    Can now determine which player wins

--- tic-tac-toe-game/src/App.jsx
+++ tic-tac-toe-game/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react"
-import GameBoard from "./components/game-board/GameBoard"
-import Players from "./components/players/Players"
+import { useState } from "react";
+
+import GameBoard from "./components/game-board/GameBoard";
+import Players from "./components/players/Players";
 import Log from "./components/game-log/Log";
 import { deriveActivePlayers } from "./utils/deriveActivePlayers";
+import { initialGameBoard, WINNING_COMBINATIONS } from "./data/data";
 
 function App() {
-  // const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayers(gameTurns);
 
+  let gameBoard = initialGameBoard;
+  let winner;
+
+  for (let turn of gameTurns) {
+    const { square, player } = turn;
+    const { rowIndex, colIndex } = square;
+
+    gameBoard[rowIndex][colIndex] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && secondSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
+
   function handlePlayerTurn(rowIndex, colIndex) {
-    // setActivePlayer(currPlayer => currPlayer === 'X' ? 'O' : 'X');
 
     setGameTurns(prevTurns => {
       const currentPlayer = deriveActivePlayers(prevTurns);
@@ -29,12 +49,13 @@ function App() {
     <main>
       <div id="game-container">
         <Players activePlayer={activePlayer} />
+        {winner && <p>{winner} wins!</p>}
         <GameBoard
           onSelectSquare={handlePlayerTurn}
-          turns={gameTurns} />
+          board={gameBoard} />
       </div>
 
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
--- tic-tac-toe-game/src/components/game-board/GameBoard.jsx
+++ tic-tac-toe-game/src/components/game-board/GameBoard.jsx
@@ -1,35 +1,7 @@
-import { initialGameBoard } from "../../data/data";
-
-export default function GameBoard({ onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard; 
-
-    //loop will not execute in there is empty array
-    for(let turn of turns) {
-       const {square, player} = turn;
-       const {rowIndex, colIndex} = square; 
-       
-       gameBoard[rowIndex][colIndex] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handlePlayerSelect(rowIndex, colIndex) {
-    //     console.log(`Button clicked at position x: ${rowIndex} y: ${colIndex}`);
-
-    //     setGameBoard((oldBoard) => {
-    //         const newBoard = [...oldBoard.map(inner => [...inner])];
-
-    //         newBoard[rowIndex][colIndex] = activePlayer;
-
-    //         return newBoard;
-    //     });
-
-    //     onSelectSquare();
-    // }
-
+export default function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => {
+            {board.map((row, rowIndex) => {
                 return (
                     <li key={rowIndex}>
                         <ol>
