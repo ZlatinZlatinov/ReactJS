6c08449aa0244ad26c1431a8e6ec56860fc0e193
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-11-02 20:29:03 +0200

    added css module

--- styling-components/src/components/Header.jsx
+++ styling-components/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import logo from '../assets/logo.png';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1 className={styles.myHeading}>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
