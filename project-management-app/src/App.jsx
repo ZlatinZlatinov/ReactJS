10c0384be957d165ceee4b7753c6816083e9cdf1
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 21:53:10 +0200

    Lifted the inital state in the main component

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -2,15 +2,30 @@ import AddProject from "./components/add-project/add-project";
 import NoProjects from "./components/no-projects/no-projects";
 import ProjectDetails from "./components/project-details/projectDetails";
 import Sidebar from "./components/sidebar/sidebar";
+
+import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom';
+import { data } from './data/data';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProjects(data);
+    }, 2000);
+  }, [data]);
+
+  function updateProjects(project) {
+    setProjects(old => [...old, project]);
+  }
+
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      <Sidebar data={projects} />
       <Routes>
         <Route path="/" element={<NoProjects />} />
-        <Route path="/add" element={<AddProject />} />
+        <Route path="/add" element={<AddProject data={projects} updateProjects={updateProjects} />} />
         <Route path="/details/:projectId" element={<ProjectDetails />} />
       </Routes>
 
