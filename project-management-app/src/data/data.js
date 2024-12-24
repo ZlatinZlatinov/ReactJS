bd0c7afafbf6db1eaa77f77e515a3b5ec5fff6c8
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 22:46:26 +0200

    Typo fixed

--- project-management-app/src/data/data.js
+++ project-management-app/src/data/data.js
@@ -2,7 +2,7 @@ export const data = [
     {
         id: 'yzfr1',
         title: "Learining React",
-        decription: "Learn React from the grup up.\nStart with the basics, finish with advanced knowledge.",
+        description: "Learn React from the grup up.\nStart with the basics, finish with advanced knowledge.",
         date: "Dec 29, 2024"
     }
 ];
