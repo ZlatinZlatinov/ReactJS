8340ee2d01205733793580b8926289519af28e53
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 17:57:58 +0200

    Updated sidebar to use Link

--- project-management-app/src/components/sidebar/sidebar.jsx
+++ project-management-app/src/components/sidebar/sidebar.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Sidebar() {
 
     return (<aside className="p-4 w-1/5 flex flex-col gap-10 bg-gray-950 text-white text-center">
         <h2 className="uppercase font-bold text-lg pt-11">Your projects</h2>
-        <button className="bg-slate-800 p-2 rounded-lg font-semibold">+ Add Project</button> 
+        <Link to="/add" className="bg-slate-800 p-2 rounded-lg font-semibold">+ Add Project</Link> 
 
         <ul>
             <li><a className="text-base">This is new created project</a></li>
         </ul>
     </aside>);
-    
 }
