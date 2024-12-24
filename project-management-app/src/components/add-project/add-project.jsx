6229971665552695cbefb92848bbb2e5fe069a89
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 18:30:59 +0200

    Updated Styling of Add-project page

--- project-management-app/src/components/add-project/add-project.jsx
+++ project-management-app/src/components/add-project/add-project.jsx
@@ -1,28 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function AddProject() {
-    return (<section>
-        <h2>Add new project</h2>
+    return (<section className="flex flex-col items-center p-10 gap-12 w-3/4 mt-5">
+        <h2 className="text-3xl font-semibold text-gray-700">Add new project</h2>
 
-        <form action="">
-        <div>
-            <button>Cancel</button>
-            <button>Save</button>
-        </div> 
+        <form className="w-full flex flex-col gap-5 mt-5">
+            <div className="self-end flex gap-2">
+                <Link to="/" className="font-semibold text-base px-8 py-3 text-center">Cancel</Link>
+                <button type="submit" className="bg-zinc-950 text-base text-white px-8 py-3 text-center rounded-lg font-semibold">Save</button>
+            </div>
 
-        <div>
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" />
-        </div> 
+            <div>
+                <label className='pb-2 block text-xl uppercase text-gray-700' htmlFor="title">Title</label>
+                <input className="w-full text-xl px-1 py-2 bg-gray-300" type="text" name="title" id="title" />
+            </div>
 
-        <div>
-            <label htmlFor="description">Description</label>
-            <textarea name="description" id="description"></textarea>
-        </div> 
+            <div>
+                <label className="pb-2 block text-xl uppercase text-gray-700" htmlFor="description">Description</label>
+                <textarea rows={5} className="w-full text-xl px-1 py-2 bg-gray-300" name="description" id="description"></textarea>
+            </div>
 
-        <div>
-            <label htmlFor="dueDate">Due Date</label> 
-            <input type="date" name="dueDate" id="dueDate" />
-        </div>
-        </form> 
+            <div>
+                <label className="pb-2 block text-xl uppercase text-gray-700" htmlFor="dueDate">Due Date</label>
+                <input className="w-full text-xl px-1 py-2 bg-gray-300" type="date" name="dueDate" id="dueDate" />
+            </div>
+        </form>
 
 
     </section>);
