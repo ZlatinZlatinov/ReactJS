74071faf67e0fb44c0da3a2aa7520202c237f33a
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 21:50:50 +0200

    Updated sidebar to accept data prop

--- project-management-app/src/components/sidebar/sidebar.jsx
+++ project-management-app/src/components/sidebar/sidebar.jsx
@@ -1,23 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { data } from '../../data/data';
 
-export default function Sidebar() {
-    const [projects, setProjects] = useState([]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setProjects(data);
-        }, 2000);
-    }, []);
+export default function Sidebar({data}) {
 
     return (<aside className="p-4 w-1/5 flex flex-col gap-10 bg-gray-950 text-white text-center">
         <h2 className="uppercase font-bold text-lg pt-11">Your projects</h2>
         <Link to="/add" className="bg-slate-800 p-2 rounded-lg font-semibold">+ Add Project</Link>
 
-        {!projects[0] ? <p>Loading...</p> :
-            <ul>
-                {projects.map((p) => {
+        {!data[0] ? <p>Loading...</p> :
+            <ul className="flex flex-col gap-3">
+                {data.map((p) => {
                     return <li key={p.id}><Link className="text-base" to={`/details/${p.id}`}>{p.title}</Link></li>
                 })}
             </ul>}
