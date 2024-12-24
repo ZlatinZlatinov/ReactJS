628a1cafe741a095a7dd98d0f6ff88f10f428f4d
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 19:13:51 +0200

    Added mock data file, too lazy to create back-end

---
+++ project-management-app/src/data/data.js
@@ -0,0 +1,8 @@
+export const data = [
+    {
+        id: 1,
+        title: "Learining React",
+        decription: "Learn React from the grup up.\nStart with the basics, finish with advanced knowledge.",
+        date: "Dec 29, 2024"
+    }
+];
