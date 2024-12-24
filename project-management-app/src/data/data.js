eb657d42bee2936e43e4f8a600afea6627c4651e
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 21:52:09 +0200

    Uptadet inital Id of project

--- project-management-app/src/data/data.js
+++ project-management-app/src/data/data.js
@@ -1,6 +1,6 @@
 export const data = [
     {
-        id: 1,
+        id: 'yzfr1',
         title: "Learining React",
         decription: "Learn React from the grup up.\nStart with the basics, finish with advanced knowledge.",
         date: "Dec 29, 2024"
