edb4c2b6e0e69e8e69924064c2dd03a027b437f1
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 17:51:57 +0200

    Updated styling of no-projects

--- project-management-app/src/components/no-projects/no-projects.jsx
+++ project-management-app/src/components/no-projects/no-projects.jsx
@@ -1,11 +1,11 @@
 import logo from '../../assets/no-projects.png';
 
 export default function NoProjects() {
-    return (<section>
-        <img src={logo} alt="No Project Selected" />
-        <h2>No Project Selected</h2>
-        <p>Select a project or get started with new one</p>
+    return (<section className='text-center flex flex-col p-20 gap-8 items-center h-full w-3/4'>
+        <img src={logo} className='w-1/4' alt="No Project Selected" />
+        <h2 className='text-xl text-gray-800 font-bold'>No Project Selected</h2>
+        <p className='text-gray-600 text-base'>Select a project or get started with new one</p>
 
-        <button>Create new Project</button>
+        <button className='bg-zinc-950 text-base text-white p-3 rounded-lg font-semibold w-1/4'>Create new project</button>
     </section>);
 }
