23ef83c761b383a1453fddaf7509d2ce0b0b90f7
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 16:53:34 +0200

    Added Routes Routing components

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,8 +1,20 @@
+import AddProject from "./components/add-project/add-project";
+import NoProjects from "./components/no-projects/no-projects";
+import ProjectDetails from "./components/project-details/projectDetails";
+import Sidebar from "./components/sidebar/sidebar";
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<NoProjects />} />
+        <Route path="/add" element={<AddProject />} />
+        <Route path="/details/:projectId" element={<ProjectDetails />} />
+      </Routes>
+
+    </main>
   );
 }
 
