abd446c3f3c937e104d17b4a9f0e0db940d06f5a
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 17:00:31 +0200

    Created Sidebar

---
+++ project-management-app/src/components/sidebar/sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+
+    return (<aside>
+        <h2>Your projects</h2>
+        <button>+ Add Project</button> 
+
+        <ul>
+            <li><a>This is new created project</a></li>
+        </ul>
+    </aside>);
+    
+}
