b3a60360972db8a814711d956c2e1bd010370170
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-24 22:40:46 +0200

    Updated project details to load proper data, add task works also

--- project-management-app/src/components/project-details/projectDetails.jsx
+++ project-management-app/src/components/project-details/projectDetails.jsx
@@ -1,27 +1,58 @@
-export default function ProjectDetails() {
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getId } from '../../utils/getRandomId';
+
+export default function ProjectDetails({ data }) {
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, inputHandler] = useState({
+        id: getId(),
+        task: ''
+    });
+
+    const { projectId } = useParams();
+
+    const project = data.find(p => p.id == projectId);
+
+    function addNewTask(e) {
+        e.preventDefault();
+
+        setTasks(old => [...old, inputValue]);
+        inputHandler({
+            id: getId(),
+            task: ''
+        });
+    }
+
     return (<section className="p-12 w-3/4 text-xl">
         <div className="mt-8 flex flex-col gap-8 py-8">
             <div className="flex w-full justify-between">
-                <h2 className="text-4xl font-bold">Learing React</h2>
+                <h2 className="text-4xl font-bold">{project.title}</h2>
                 <button className="hover:text-red-800">Delete</button>
             </div>
 
-            <p className="text-gray-500">Dec 29, 2024</p>
-            <p>Learn from the ground up....</p>
+            <p className="text-gray-500">{project.date}</p>
+            <p>{project.description}</p>
         </div>
 
         <span className="h-1 w-full block bg-gray-300"> </span>
 
         <div className="mt-5 flex flex-col gap-6">
             <h3 className="text-3xl font-bold">Tasks</h3>
-            <form className="flex gap-3 w-full">
-                <input className="bg-gray-200 w-1/4 text-lg py-1" type="text" />
+
+            <form className="flex gap-3 w-full" onSubmit={addNewTask}>
+                <input
+                    className="bg-gray-200 w-1/4 text-lg py-1"
+                    type="text"
+                    name='task'
+                    id='task'
+                    value={inputValue.task}
+                    onChange={(e) => inputHandler(t => ({ ...t, 'task': e.target.value }))} />
                 <button type="submit">Add task</button>
             </form>
 
             <ul className="flex flex-col gap-4 w-full">
-                <li className="flex justify-between bg-gray-200 px-4 py-2"><span>Practice, practice</span><button className="hover:text-red-800">Clear</button></li>
-                <li className="flex justify-between bg-gray-200 px-4 py-2"><span>Practice, practice</span><button className="hover:text-red-800">Clear</button></li>
+                {!tasks[0] ? <li className="flex justify-between bg-gray-200 px-4 py-2"><span>No tasks yet!</span></li> :
+                    tasks.map(t => <li key={t.id} className="flex justify-between bg-gray-200 px-4 py-2"><span>{t.task}</span><button className="hover:text-red-800">Clear</button></li>)}
             </ul>
         </div>
     </section>)
