29c9bf2c7b3376bfe4622b02fe08ce76c67c67d0
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-25 22:35:32 +0200

    Forgot to add the updated sidebar

--- project-management-app/src/components/sidebar/sidebar.jsx
+++ project-management-app/src/components/sidebar/sidebar.jsx
@@ -1,7 +1,22 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { getAllProjects } from "../../services/projectService";
 
-export default function Sidebar({data}) {
+export default function Sidebar() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function getProjects() {
+            try {
+                const result = await getAllProjects();
+                setData(result);
+            } catch (err) {
+                alert(err.message);
+            }
+        }
+
+        getProjects();
+    }, []);
 
     return (<aside className="p-4 w-1/5 flex flex-col gap-10 bg-gray-950 text-white text-center">
         <h2 className="uppercase font-bold text-lg pt-11">Your projects</h2>
@@ -10,7 +25,7 @@ export default function Sidebar({data}) {
         {!data[0] ? <p>Loading...</p> :
             <ul className="flex flex-col gap-3">
                 {data.map((p) => {
-                    return <li key={p.id}><Link className="text-base" to={`/details/${p.id}`}>{p.title}</Link></li>
+                    return <li key={p._id}><Link className="text-base" to={`/details/${p._id}`}>{p.title}</Link></li>
                 })}
             </ul>}
     </aside>);
